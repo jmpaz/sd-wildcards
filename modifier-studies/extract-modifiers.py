39c936bdf428f62f97516d6cfa9a981cfd037ba8
Josh <josh@pazmi.no>
2022-10-31 04:32:34 -0400

    Create extract-modifiers.py

---
+++ modifier-studies/extract-modifiers.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import csv
+import os
+import json
+import re
+
+modifiers = {}
+
+# Create the directory structure if it doesn't already exist
+if not os.path.exists('modifiers'):
+    os.makedirs('modifiers')
+
+if not os.path.exists('modifiers/strength'):
+    os.makedirs('modifiers/strength')
+
+if not os.path.exists('modifiers/tags'):
+    os.makedirs('modifiers/tags')
+
+
+# Get column indexes for Name, Strength, and Tags from the first row of the csv file
+with open('source.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    headers = next(reader)
+    name_index = headers.index('Name')
+    strength_index = headers.index('Strength')
+    tags_index = headers.index('Tags')
+
+    # Iterate through each row of the csv file starting after column headers
+    for row in reader:
+
+        # Get the Name, Strength, and Tags values from each row
+        name = row[name_index]
+        strength = row[strength_index]
+        tags = row[tags_index]
+
+        # Add the Name, Strength, and Tags values to the modifiers dictionary if they don't already exist
+        if name not in modifiers:
+            modifiers[name] = {
+                'strength': strength,
+                'tags': []
+            }
+
+        # Add the Tags values to the modifiers dictionary if they don't already exist
+        for tag in tags.split(','):
+            tag = tag.strip()
+
+            if tag not in modifiers[name]['tags']:
+                modifiers[name]['tags'].append(tag)
+
+        # Create a new text file for each tag if it doesn't already exist
+        for tag in modifiers[name]['tags']:
+
+            # Remove any non-alphanumeric characters from the filename, replacing them with a hyphen
+            tag_filename = re.sub(r'[^a-zA-Z0-9]', '-', tag)
+
+            if not os.path.exists('modifiers/tags/{}.txt'.format(tag_filename)):
+                with open('modifiers/tags/{}.txt'.format(tag_filename), 'w') as tagfile:
+                    pass
+
+        # Append the Name value to the corresponding text file for each tag
+        for tag in modifiers[name]['tags']:
+
+            # Remove any non-alphanumeric characters from the filename, replacing them with a hyphen
+            tag_filename = re.sub(r'[^a-zA-Z0-9]', '-', tag)
+
+            with open('modifiers/tags/{}.txt'.format(tag_filename), 'a') as tagfile:
+                print(name, file=tagfile)
+
+        # Append the Name value to the corresponding text file for each strength level if it doesn't already exist
+        if strength == '★★★':
+            strength_filename = 'high'
+
+        elif strength == '★★☆':
+            strength_filename = 'med'
+
+        elif strength == '★☆☆':
+            strength_filename = 'low'
+
+        with open('modifiers/strength/{}.txt'.format(strength_filename), 'a') as strengthfile:
+            print(name, file=strengthfile)
+
+
+# Create text and json files containing all Name values from the csv
+with open('modifiers.txt', 'w') as modifiersfile:
+    for name in modifiers:
+        print(name, file=modifiersfile)
+
+with open('modifiers.json', 'w') as jsonfile:
+    json.dump(modifiers, jsonfile, indent=2)
+    
+print('Done!')
